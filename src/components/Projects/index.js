a8ae67788b8d1bbc747eedb86a06675e129c80aa
PrakarshKamal <kamalp@mcmaster.ca>
2022-12-14 12:57:51 -0500

    placeholder for projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import './index.scss'
 import Loader from 'react-loaders'
 
 const Project = () => {
   return (
     <>
-      <div>Project</div>
+      <div className="project-container">
+        <h1>Coming soon!</h1>
+      </div>
       <Loader type="ball-rotate" />
     </>
   )
