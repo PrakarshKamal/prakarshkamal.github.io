b1a3fe9b3f7cca0bcd02355d47f93e656afd0076
PrakarshKamal <kamalp@mcmaster.ca>
2022-12-14 01:50:27 -0500

    added Projects page

---
+++ src/components/Projects/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Loader from 'react-loaders'
+
+const Project = () => {
+  return (
+    <>
+      <div>Project</div>
+      <Loader type="ball-rotate" />
+    </>
+  )
+}
+
+export default Project
