c07ec282e8e5b418e28830b20f354cb182f884e9
PrakarshKamal <kamalp@mcmaster.ca>
2023-01-04 21:36:48 -0500

    add project

--- src/components/Projects/Card/CardData.js
+++ src/components/Projects/Card/CardData.js
@@ -1,8 +1,16 @@
+import news from '../../../assets/images/news.png'
 import hydronizer from '../../../assets/images/hydronizer.png'
 import gcal from '../../../assets/images/gcal.png'
 import user from '../../../assets/images/user.png'
 
 const data = [
+  {
+    image: news,
+    title: 'The Daily Edition',
+    description:
+      'A news app with a clean UI pulling lives news from the mediastack API',
+    link: 'https://github.com/PrakarshKamal/the-daily-edition',
+  },
   {
     image: hydronizer,
     title: 'Hydronizer',
@@ -21,7 +29,7 @@ const data = [
     image: user,
     title: 'Social Media App',
     description:
-      'Social media site with login page, friend requests, creating/liking posts.',
+      'A Social media site with login page, friend requests, creating/liking posts.',
     link: 'https://github.com/PrakarshKamal/social-media-clone',
   },
 ]
