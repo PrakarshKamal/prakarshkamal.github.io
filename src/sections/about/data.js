e8e31f34316b54cd28f32d6d92a1a14d1222af8b
PrakarshKamal <kamalp@mcmaster.ca>
2023-02-01 20:00:43 -0500

    data for about

---
+++ src/sections/about/data.js
@@ -0,0 +1,16 @@
+import { FaAward } from "react-icons/fa";
+import { ImBooks } from "react-icons/im";
+import { FaKeyboard } from "react-icons/fa";
+
+const data = [
+  {
+    id: 1,
+    icon: <FaAward />,
+    title: "Experience",
+    desc: "1+ Years Working",
+  },
+  { id: 2, icon: <ImBooks />, title: "Projects", desc: "10+ Completed" },
+  { id: 3, icon: <FaKeyboard />, title: "Keystrokes", desc: "85 WPM" },
+];
+
+export default data;
