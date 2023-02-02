da3461cf84685c98c87400a9b1081fdfe6460901
PrakarshKamal <kamalp@mcmaster.ca>
2023-02-01 19:55:56 -0500

    header components + data

---
+++ src/sections/header/Header.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import HeaderPortrait from "../../assets/profile.jpg";
+import data from "./data";
+import "./Header.css";
+
+const Header = () => {
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
+
+  return (
+    <header id="header">
+      <div className="container header__container">
+        <div className="header__portrait" data-aos="zoom-in">
+          <img src={HeaderPortrait} alt="" />
+        </div>
+        <h2 data-aos="fade-up">Prakarsh Kamal</h2>
+        <p data-aos="fade-up">Software Developer | Student | Learner</p>
+        <div className="header__cta" data-aos="fade-up">
+          <a href="#contact" className="button primary">
+            Contact Me
+          </a>
+          <a href="#portfolio" className="button light">
+            My Portfolio
+          </a>
+        </div>
+        <div className="header__socials">
+          {data.map((item) => (
+            <a
+              key={item.id}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          ))}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/sections/header/data.js
@@ -0,0 +1,17 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+
+const data = [
+  {
+    id: 1,
+    link: "https://www.linkedin.com/in/prakarsh-kamal/",
+    icon: <FaLinkedin />,
+  },
+  { id: 2, link: "https://github.com/PrakarshKamal", icon: <FaGithub /> },
+  { id: 3, link: "mailto:prakarshkamal@gmail.com", icon: <SiGmail /> },
+  { id: 4, link: "https://www.instagram.com/pangkaxd/", icon: <FaInstagram /> },
+];
+
+export default data;
