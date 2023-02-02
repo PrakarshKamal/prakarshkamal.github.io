ad2751197fdca70803fb3b2165ca12c3d99f42f3
PrakarshKamal <kamalp@mcmaster.ca>
2023-02-01 20:00:07 -0500

    contact component

---
+++ src/sections/contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <h2>Contact</h2>
+    </section>
+  );
+};
+
+export default Contact;
